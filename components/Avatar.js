12406a6a43cea4154aa88e5d3be2ac76958321e8
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2024-04-24 21:31:14 -0500

    possible fix/ not sure

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,18 @@
+// next image
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src={"/avatarSY.png"}
+        width={637}
+        height={678}
+        alt=""
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,32 @@
+import Link from "next/link";
+
+// icons
+import { RiInstagramLine, RiFacebookLine } from "react-icons/ri";
+import { FaLinkedin } from "react-icons/fa";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={"https://www.linkedin.com/in/yociusproductions/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <FaLinkedin />
+      </Link>
+      <Link
+        href={"https://www.facebook.com/contactYociusProductions"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiFacebookLine />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/yociusproductions/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
